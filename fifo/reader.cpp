fe0f058037bb8cf4996044d99c10e185e28487f9
Maxim Davydov <Davydov.MA@phystech.edu>
2019-09-26 02:53:39 +0300

    First version. It works only if you start reader and then writer

---
+++ fifo/reader.cpp
@@ -0,0 +1,99 @@
+//
+// Created by max on 26.09.19.
+//
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <assert.h>
+
+
+ssize_t calcSize(const char* inFile);
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Error num_params\n");
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    int in_fd = open(argv[1], O_RDONLY);
+    if (in_fd < 0)
+    {
+        printf("Error of open reading file = %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t file_size = calcSize(argv[1]);
+
+    errno = 0;
+    int ret_stat = mkfifo("fifo.p", 00600);
+    if (ret_stat < 0)
+    {
+        printf("mkfifo error - %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    int fifo_fd = open("fifo.p", O_WRONLY);
+    if (fifo_fd < 0)
+    {
+        printf("Error of opening fifo = %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    char* buff = (char*) calloc(file_size, sizeof(buff[0]));
+    if (buff == NULL || errno != 0)
+    {
+        printf("Memory error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    ssize_t ret_val = read(in_fd, buff,file_size * sizeof(buff[0]));
+    if (ret_val < 0)
+    {
+        printf("Read error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    close(in_fd);
+
+    errno = 0;
+    ret_val = write(fifo_fd, &file_size, sizeof(file_size));
+    if (ret_val < 0)
+    {
+        printf("Write size error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    ret_val = write(fifo_fd, buff, file_size * sizeof(buff[0]));
+    if (ret_val < 0)
+    {
+        printf("Write error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    close(fifo_fd);
+    free(buff);
+
+    return 0;
+}
+
+ssize_t calcSize(const char* inFile)
+{
+    assert(inFile != nullptr);
+
+    struct stat fileInfo;
+    stat(inFile, &fileInfo);
+
+    return fileInfo.st_size;
+}
---
+++ fifo/writer.cpp
@@ -0,0 +1,58 @@
+//
+// Created by max on 26.09.19.
+//
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+
+int main()
+{
+    errno = 0;
+    int fifo_fd = open("fifo.p", O_RDONLY);
+    if (fifo_fd < 0)
+    {
+        printf("Open fifo error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    size_t size_buff = 0;
+    errno = 0;
+    ssize_t ret_val = read(fifo_fd, &size_buff, sizeof(size_buff));
+    if (ret_val < 0)
+    {
+        printf("Read size error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    char* buff = (char*) calloc(size_buff, sizeof(buff[0]));
+    if (buff == NULL || errno != 0)
+    {
+        printf("Memory error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    ret_val = read(fifo_fd, buff, size_buff * sizeof(buff[0]));
+    if (ret_val < 0)
+    {
+        printf("Read error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    ret_val = write(STDOUT_FILENO, buff, size_buff * sizeof(buff[0]));
+    if (ret_val < 0)
+    {
+        printf("Print error = %d", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    free(buff);
+    close(fifo_fd);
+
+    return 0;
+}
