cb7fe445e83a9727526e2eb076e5423cf24c767f
Maxim Davydov <Davydov.MA@phystech.edu>
2019-11-23 17:59:30 +0300

    SEM_UNDO initialization in writer

--- sh_mem/reader.cpp
+++ sh_mem/reader.cpp
@@ -9,10 +9,19 @@
 #include <string.h>
 #include <sys/ipc.h>
 
+enum {
+    start   = 0,
+    mutex   = 1,
+    full    = 2,
+    alive_r = 3,
+    alive_w = 4,
+    die     = 5
+};
+
 int main()
 {
     errno = 0;
-    key_t key = ftok("/home/max/.profile", 1);
+    key_t key = ftok("/home/max/.profile", 1);//
     if (key < 0)
     {
         perror("key gen");
@@ -28,7 +37,7 @@ int main()
     }
 
     errno = 0;
-    char* sh_mem = (char*) shmat(shm_id, NULL, 0);
+    char* sh_mem = (char*) shmat(shm_id, NULL, 0);//
     if (sh_mem == NULL)
     {
         perror("Attach mem error");
@@ -38,22 +47,17 @@ int main()
     char* buff = (char*) calloc(4096, sizeof(buff[0]));
 
     errno = 0;
-    int sem_id = semget(key, 7, IPC_CREAT | 0600);
+    int sem_id = semget(key, 6, IPC_CREAT | 0600);
     if (sem_id < 0 )
     {
         perror("Sem get/create error");
         exit(EXIT_FAILURE);
     }
 
-    struct sembuf sops[2];
-
-    sops[0].sem_num = 0;
-    sops[0].sem_op = 0;
-    sops[0].sem_flg = IPC_NOWAIT;
-
-    sops[1].sem_num = 0;
-    sops[1].sem_op = 1;
-    sops[1].sem_flg = 0;
+    struct sembuf sops[2] = {
+        {start, 0, IPC_NOWAIT},
+        {start, 1, 0}
+    };// SEM_UNDO
 
     errno = 0;
     int ret = semop(sem_id, sops, 2);
@@ -65,16 +69,17 @@ int main()
 
     if (ret >= 0)
     {
-        sembuf sops2[5] = {
-            {1, 1, 0},
-            {3, 4096, 0},
-            {4, 1, 0},
-            {5, 1, 0},
-            {6, 2, 0}
+        printf("I'm inicializer\n");
+        fflush(0);
+        sembuf sops2[4] = {
+            {mutex, 1, 0},
+            {alive_r, 1, 0},
+            {alive_w, 1, 0},
+            {die, 2, 0}
           };
 
         errno = 0;
-        ret = semop(sem_id, sops2, 5);
+        ret = semop(sem_id, sops2, 4);
         if (ret < 0)
         {
             perror("Initialization sem error");
@@ -83,23 +88,28 @@ int main()
     }
 ////////////////////////////////////////////////////////////////////////////////
     sembuf sops3[3] = {
-        {4, -1, SEM_UNDO},
-        {6, -2, 0},
-        {6, 2, 0}
-    };
+        {die, -2, IPC_NOWAIT},
+        {die, 2, 0},
+        {alive_r, -1, SEM_UNDO},
+    };// maybe kill
 
     errno = 0;
     ret = semop(sem_id, sops3, 3);
-    if (ret < 0)
+    if (ret < 0 && errno != EAGAIN)
     {
         perror("Reader sem init error");
         exit(EXIT_FAILURE);
     }
+    if (errno == EAGAIN)
+    {
+        perror("I deied while waiting");
+        exit(EXIT_FAILURE);
+    }
 
     sembuf sops4[2] = {
-      {5, 0, 0},
-      {6, -1, SEM_UNDO}
-  };
+      {alive_w, 0, 0},
+      {die, -1, SEM_UNDO}
+    };
     errno = 0;
     ret = semop(sem_id, sops4, 2);
     if (ret < 0)
@@ -108,43 +118,54 @@ int main()
         exit(EXIT_FAILURE);
     }
 ////////////////////////////////////////////////////////////////////////////////
+    errno = 0;
+    sembuf sops5 = {die, 0, 0};
+    ret = semop(sem_id, &sops5, 1);
+    if (ret < 0)
+    {
+        perror("Wait love error");
+        exit(EXIT_FAILURE);
+    }
+////////////////////////////////////////////////////////////////////////////////
     ssize_t ret_write = 0;
-
+    ssize_t ret_read = 0;
     do
     {
-        sembuf sops5 = {6, 0, IPC_NOWAIT};
+        sops5 = {die, 0, IPC_NOWAIT};
+        errno = 0;
         ret = semop(sem_id, &sops5, 1);
-        if (ret < 0)
+        if (ret < 0 && errno != EAGAIN)
         {
             perror("Die brother error");
             exit(EXIT_FAILURE);
         }
+        if (ret < 0 && errno == EAGAIN)
+            break;
 
-        sembuf sops6[2] = {
-          {2, -4096, SEM_UNDO},
-          {1, -1, SEM_UNDO}
+        sembuf sops6[3] = {
+            {die, 0, IPC_NOWAIT},
+            {full, -1, 0},
+            {mutex, -1, SEM_UNDO},
         };
 
         errno = 0;
-        ret = semop(sem_id, sops6, 2);
-        if (ret < 0)
+        ret = semop(sem_id, sops6, 3);
+        if (ret < 0 && errno != EAGAIN)
         {
-            perror("Sem mutex writer error");
+            perror("Take mutex reader error");
             exit(EXIT_FAILURE);
         }
+        if (ret < 0 && errno == EAGAIN)
+            break;
 
-        memcpy(buff, sh_mem, 4096);
+        memcpy(buff, sh_mem, 4096);//
 
-        sembuf sops7[2] = {
-          {3, 4096, SEM_UNDO},
-          {1, 1, SEM_UNDO}
-        };
-
+        sembuf sops7 = {mutex, 1, SEM_UNDO};
         errno = 0;
-        ret = semop(sem_id, sops7, 2);
+        ret = semop(sem_id, &sops7, 1);
         if (ret < 0)
         {
-            perror("Sem mutex writer error");
+            perror("Set mutex reader error");
             exit(EXIT_FAILURE);
         }
 
@@ -155,12 +176,14 @@ int main()
             perror("Out write error");
             exit(EXIT_FAILURE);
         }
+        printf("%ld\n", ret_write);
+        fflush(0);
     } while(ret_write);
 
     free(buff);
 
     errno = 0;
-    ret = shmdt(sh_mem);
+    ret = shmdt(sh_mem);//
     if (ret < 0)
     {
         perror("Dettach error");
@@ -168,10 +191,18 @@ int main()
     }
 
     errno = 0;
-    ret = shmctl(shm_id, IPC_RMID, NULL);
+    ret = shmctl(shm_id, IPC_RMID, NULL);//
     if (ret < 0)
     {
         perror("RM_shmem error");
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    ret = semctl(sem_id, 0, IPC_RMID);//
+    if (ret < 0)// && != RM
+    {
+        perror("Rm sem error");
         exit(EXIT_FAILURE);
     }
 
--- sh_mem/writer.cpp
+++ sh_mem/writer.cpp
@@ -9,6 +9,15 @@
 #include <string.h>
 #include <sys/ipc.h>
 
+enum {
+    start   = 0,
+    mutex   = 1,
+    full    = 2,
+    alive_r = 3,
+    alive_w = 4,
+    die     = 5
+};
+
 int main(int argc, char* argv[])
 {
     if (argc != 2)
@@ -26,7 +35,7 @@ int main(int argc, char* argv[])
     }
 
     errno = 0;
-    key_t key = ftok("/home/max/.profile", 1);
+    key_t key = ftok("/home/max/.profile", 1);//
     if (key < 0)
     {
         perror("key gen");
@@ -43,7 +52,7 @@ int main(int argc, char* argv[])
 
     errno = 0;
     char* sh_mem = (char*) shmat(shm_id, NULL, 0);
-    if (sh_mem == NULL)
+    if (*sh_mem == -1)
     {
         perror("Attach mem error");
         exit(EXIT_FAILURE);
@@ -52,22 +61,17 @@ int main(int argc, char* argv[])
     char* buff = (char*) calloc(4096, sizeof(buff[0]));
 
     errno = 0;
-    int sem_id = semget(key, 7, IPC_CREAT | 0600);
+    int sem_id = semget(key, 6, IPC_CREAT | 0600);
     if (sem_id < 0)
     {
         perror("Sem get/create error");
         exit(EXIT_FAILURE);
     }
 
-    struct sembuf sops[2];
-
-    sops[0].sem_num = 0;
-    sops[0].sem_op = 0;
-    sops[0].sem_flg = IPC_NOWAIT;
-
-    sops[1].sem_num = 0;
-    sops[1].sem_op = 1;
-    sops[1].sem_flg = 0;
+    struct sembuf sops[2] = {
+        {start, 0, IPC_NOWAIT},
+        {start, 1, 0}
+    };// SEM_UNDO
 
     errno = 0;
     int ret = semop(sem_id, sops, 2);
@@ -79,41 +83,49 @@ int main(int argc, char* argv[])
 
     if (ret >= 0)
     {
-        sembuf sops2[5] = {
-            {1, 1, 0},
-            {3, 4096, 0},
-            {4, 1, 0},
-            {5, 1, 0},
-            {6, 2, 0}
+        printf("I'm inicializer\n");
+        fflush(0);
+
+        sembuf sops2[4] = {
+            {mutex, 1, 0},
+            {alive_r, 1, 0},
+            {alive_w, 1, 0},
+            {die, 2, 0}
           };
 
         errno = 0;
-        ret = semop(sem_id, sops2, 5);
+        ret = semop(sem_id, sops2, 4);
         if (ret < 0)
         {
             perror("Initialization sem error");
             exit(EXIT_FAILURE);
         }
     }
-
-    ////////////////////////////////////////////////////////////////////////////
-    sembuf sops3[3] = {
-        {5, -1, SEM_UNDO},/*Del*/
-        {6, -2, 0},
-        {6, 2, 0}
+////////////////////////////////////////////////////////////////////////////////
+    sembuf sops3[5] = {
+        {die, -2, IPC_NOWAIT},
+        {die, 2, 0},
+        {alive_w, -1, SEM_UNDO},/*Del*/
+        {full, 1, SEM_UNDO},
+        {full, -1, 0}
     };
 
     errno = 0;
-    ret = semop(sem_id, sops3, 3);
-    if (ret < 0)
+    ret = semop(sem_id, sops3, 5);
+    if (ret < 0 && errno != EAGAIN)
     {
         perror("Sem enter writer error");
         exit(EXIT_FAILURE);
     }
+    if (errno == EAGAIN)
+    {
+        perror("I died while waiting");
+        exit(EXIT_FAILURE);
+    }
 
     sembuf sops4[2] = {
-      {4, 0, 0},
-      {6, -1, SEM_UNDO}
+      {alive_r, 0, 0},
+      {die, -1, SEM_UNDO},
   };
     errno = 0;
     ret = semop(sem_id, sops4, 2);
@@ -122,20 +134,28 @@ int main(int argc, char* argv[])
         perror("Sem die writer error");
         exit(EXIT_FAILURE);
     }
-    ////////////////////////////////////////////////////////////////////////////
-
+////////////////////////////////////////////////////////////////////////////////
+    errno = 0;
+    sembuf sops5 = {die, 0, 0};
+    ret = semop(sem_id, &sops5, 1);
+    if (ret < 0)
+    {
+        perror("Wait love error");
+        exit(EXIT_FAILURE);
+    }
+////////////////////////////////////////////////////////////////////////////////
     ssize_t ret_read = 0;
     do
     {
         errno = 0;
         ssize_t ret_read = read(fd, buff, 4096);
-        if (ret < 0)
+        if (ret_read < 0)
         {
             perror("Read file error");
             exit(EXIT_FAILURE);
         }
 
-        sembuf sops5 = {6, 0, IPC_NOWAIT};
+        sops5 = {die, 0, IPC_NOWAIT};
         errno = 0;
         ret = semop(sem_id, &sops5, 1);
         if (ret < 0)
@@ -143,34 +163,40 @@ int main(int argc, char* argv[])
             perror("Sem check die writer error");
             exit(EXIT_FAILURE);
         }
-
-        sembuf sops6[2] = {
-          {3, -4096, SEM_UNDO},
-          {1, -1, SEM_UNDO}
-      };
+        //exit(0);
+        sembuf sops6[3] = {
+            {die, 0, IPC_NOWAIT},
+            {full, 0, 0},
+            {mutex, -1, SEM_UNDO},
+       };
 
         errno = 0;
-        ret = semop(sem_id, sops6, 2);
+        ret = semop(sem_id, sops6, 3);
         if (ret < 0)
         {
-            perror("Sem mutex writer error");
+            perror("Take mutex writer error");
             exit(EXIT_FAILURE);
         }
 
+        memset(sh_mem, '\0', 4096);
         memcpy(sh_mem, buff, 4096);
-
+        //exit(0);
         sembuf sops7[2] = {
-          {2, 4096, SEM_UNDO},
-          {1, 1, SEM_UNDO}
-      };
+          {full, 1, 0},// undo to 0
+          {mutex, 1, SEM_UNDO}
+       };
 
         errno = 0;
         ret = semop(sem_id, sops7, 2);
         if (ret < 0)
         {
-            perror("Sem mutex writer error");
+            perror("Return mutex writer error");
             exit(EXIT_FAILURE);
         }
+
+        //exit(0);
+        //printf("%ld\n", ret_read);
+        //fflush(0);
 
     } while(ret_read);
 ////////////////////////////////////////////////////////////////////////////////
@@ -180,20 +206,48 @@ int main(int argc, char* argv[])
     close(fd);
 
     errno = 0;
+    sembuf sops8[2] = {
+        {full, 0, 0},
+        {die, 0, IPC_NOWAIT}
+    };
+    ret = semop(sem_id, sops8, 2);
+    if (ret < 0)
+    {
+        perror("Wait for ending error");
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
     ret = shmdt(sh_mem);
     if (ret < 0)
     {
         perror("Dettach error");
         exit(EXIT_FAILURE);
     }
-
-    errno = 0;
+    //exit(0);
+    /*errno = 0;
     ret = shmctl(shm_id, IPC_RMID, NULL);
     if (ret < 0)
     {
         perror("RM_shmem error");
         exit(EXIT_FAILURE);
+    }*/
+
+    /*sembuf sops9 = {die, 1, SEM_UNDO};
+    ret = semop(sem_id, &sops9, 1);
+    if (ret < 0)
+    {
+        perror("Set die error");
+        exit(EXIT_FAILURE);
     }
+
+    errno = 0;
+    ret = semctl(sem_id, 0, IPC_RMID);
+    if (ret < 0)
+    {
+        perror("Rm sem error");
+        exit(EXIT_FAILURE);
+    }*/
 
     return 0;
 }
