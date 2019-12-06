dc42172f22c206ebb87518fc0488e4d3566cf1b2
Maxim Davydov <Davydov.MA@phystech.edu>
2019-12-06 17:30:32 +0300

    Fifo passed

--- fifo/func.c
+++ fifo/func.c
@@ -31,9 +31,10 @@ int main()
 	mkfifo("fifo", 666);
 	int fd = open("fifo", O_RDWR);
 	write(fd, "aaa", 3);
-	close(fd);
 
-	fd = open("fifo", O_RDONLY, O_NONBLOCK);
+	//close(fd);
+
+	fd = open("fifo", O_RDONLY | O_NONBLOCK);
 	write(1, buf, read(fd, buf, 3));
 	return 0;
 }
